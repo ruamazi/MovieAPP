234e4806433ca818dc1879ec51e6f2b895a3c047
ruamazi <119816587+ruamazi@users.noreply.github.com>
2023-09-03 16:39:00 +0100

    Update Footer.jsx
    o

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,14 +6,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <h5>Website Coded By Mohssin Aoulad</h5>
-      <div className="wts-cont">
-        <img
-          className="wts-icon"
-          src="https://cdn-icons-png.flaticon.com/512/2504/2504845.png"
-          alt=""
-        />
-        <p>+212 624-643204</p>
-      </div>
+
       <p className="copyright-text">© {currentYear} All rights reserved</p>
     </div>
   );
